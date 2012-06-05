857d730b78ab31c5baa40bc4951c97bfc4d0b24c
tuser <tuser@tuser-VirtualBox.(none)>
2012-06-05 15:12:32 +0100

    Structure in place for exercises

---
+++ 01-LinuxCommands/features/bootstrap/FeatureContext.php
@@ -0,0 +1,43 @@
+<?php
+
+use Behat\Behat\Context\BehatContext,
+    Behat\Behat\Exception\PendingException;
+use Behat\Gherkin\Node\PyStringNode,
+    Behat\Gherkin\Node\TableNode;
+
+class FeatureContext extends BehatContext
+{
+    private $output;
+
+    /** @Given /^I am in a directory "([^"]*)"$/ */
+    public function iAmInADirectory($dir)
+    {
+        if (!file_exists($dir)) {
+            mkdir($dir);
+        }
+        chdir($dir);
+    }
+
+    /** @Given /^I have a file named "([^"]*)"$/ */
+    public function iHaveAFileNamed($file)
+    {
+        touch($file);
+    }
+
+    /** @When /^I run "([^"]*)"$/ */
+    public function iRun($command)
+    {
+        exec($command, $output);
+        $this->output = trim(implode("\n", $output));
+    }
+
+    /** @Then /^I should get:$/ */
+    public function iShouldGet(PyStringNode $string)
+    {
+        if ((string) $string !== $this->output) {
+            throw new Exception(
+                "Actual output is:\n" . $this->output
+            );
+        }
+    }
+}
